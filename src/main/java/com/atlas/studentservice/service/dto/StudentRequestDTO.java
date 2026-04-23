c4bd0b7a0ba14350e8c4519cf829ddd78fa49229
pushpa-java14 <pushpanellore30@gmail.com>
2026-04-23 21:46:22 +0530

    SCRUM-18 implement request and responce for student

---
+++ src/main/java/com/atlas/studentservice/service/dto/StudentRequestDTO.java
@@ -0,0 +1,70 @@
+package com.atlas.studentservice.service.dto;
+
+import java.time.LocalDate;
+
+public class StudentRequestDTO {
+	private Long studentID;
+	private String firstName;
+	private String lastName;
+	private String emailId;
+	private String password;
+	private String contactNumber;
+	private LocalDate dateOfBirth;
+
+	public Long getStudentID() {
+		return studentID;
+	}
+
+	public void setStudentID(Long studentID) {
+		this.studentID = studentID;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getContactNumber() {
+		return contactNumber;
+	}
+
+	public void setContactNumber(String contactNumber) {
+		this.contactNumber = contactNumber;
+	}
+
+	public LocalDate getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(LocalDate dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+}
