fefcafdc12588393b9fd3b6f54de30f792b986b5
pushpa-java14 <pushpanellore30@gmail.com>
2026-04-23 22:09:48 +0530

    SCRUM-18 implemented request and responce for student

--- src/main/java/com/atlas/studentservice/service/dto/StudentRequestDTO.java
+++ src/main/java/com/atlas/studentservice/service/dto/StudentRequestDTO.java
@@ -2,22 +2,40 @@ package com.atlas.studentservice.service.dto;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 public class StudentRequestDTO {
-	private Long studentID;
+
+	@NotBlank(message = "firstName is required")
+	@Size(min = 3, max = 50, message = "firstName must be between 3 and 50 character")
 	private String firstName;
+
+	@NotBlank(message = "lastName is required")
+	@Size(min = 3, max = 50, message = "firstName must be between 3 and 50 character")
 	private String lastName;
+
+	@NotBlank(message = "email is required")
+	@Email(message = "Invaid email formate")
 	private String emailId;
+
+	@NotBlank(message = "password is required")
+	@Size(min = 8, max = 20, message = "password must be 8 and 20 characters")
+	@Pattern(regexp = "^(?=.[A-2](?=.[a-z](?=\\\\.d)(?=.[@#$%^&+=!]).*$\", message = \"Password must contain uppercase,lowercase,number and special caracter")
 	private String password;
+
+	@NotBlank(message = "contactNumber is required")
+	@Pattern(regexp = "^[6-9]\\d{9}$", message = "Invalid phone numbers") // Validates
 	private String contactNumber;
+
+	@NotNull(message = "dateOfBirth is required")
+	@JsonFormat(pattern = "yyyy-MM-dd")
 	private LocalDate dateOfBirth;
-
-	public Long getStudentID() {
-		return studentID;
-	}
-
-	public void setStudentID(Long studentID) {
-		this.studentID = studentID;
-	}
 
 	public String getFirstName() {
 		return firstName;
